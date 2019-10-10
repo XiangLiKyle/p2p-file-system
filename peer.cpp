8cf8ee44d7efc6bfadecfb439be3b6711e3f5c25
Adam_L <zjc5220@psu.edu>
2019-10-09 22:25:36 -0400

    received v1

--- peer.cpp
+++ peer.cpp
@@ -1,15 +1,13 @@
-#include<iostream>
-#include<unistd.h> //for close
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<memory.h>
-#include<netdb.h>
-#include<arpa/inet.h>
-#include<netinet/ip.h>
-#include<netinet/tcp.h> //for IPROTO_TCP
-#include"serversocket.h"
-#include<cstdlib>
-#include<cstring>
+#include <stdio.h>
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <fstream>
+#include <algorithm>
+#include <cstdio>
+#include <winsock2.h>
+#include <map>
+#include "serversocket.h"
 using namespace std;
 
 #define MAX_FILE_NAME = 200;
@@ -19,10 +17,12 @@ using namespace std;
 #define MAX_CHUNK = 100;
 #define MAX_TOTAL_MESSAGE = 20000;
 
-long long max_buf_size;
+map<string, int> filemap;
 int peer_file_num;
 int m_server;
 int target_peer;
+vector<string> sp;
+
 
 struct register_requset
 {
@@ -48,6 +48,23 @@ struct file_chuck_request
 	int chunk_num;
 };
 
+void SplitString(const string& s, vector<std::string>& v, const string& c)
+{
+  string::size_type pos1, pos2;
+  pos2 = s.find(c);
+  pos1 = 0;
+  v.clear();
+
+  while(std::string::npos != pos2)
+  {
+	v.push_back(s.substr(pos1, pos2-pos1));
+ 
+	pos1 = pos2 + c.size();
+	pos2 = s.find(c, pos1);
+  }
+  if(pos1 != s.length())
+	v.push_back(s.substr(pos1));
+}
 
 
 void init_request(int request_num)
@@ -59,6 +76,8 @@ void init_request(int request_num)
 	file_location_request flr;
 	chunk_register_request crr;
 	file_chuck_request fcr;
+	char recv_buffer[1024];
+	int len;
 
 	if(request_num == 1)
 	{
@@ -72,7 +91,14 @@ void init_request(int request_num)
 		}
 		server.send(m_server,total_message,strlen(total_message));
 
-		
+
+		//receive message
+		len = server.Recv(m_server,recv_buffer);
+		if(recv_buffer == "1")
+		{
+			cout << "Register request success!" << endl;
+		}
+		//next step
 	}
 
 	if(request_num = 2)
@@ -81,6 +107,20 @@ void init_request(int request_num)
 		cout << "File list requested!" << endl;
 		total_message = sprintf(num_buffer,"%d%s",request_num,"/0");
 		server.send(m_server,total_message,2);
+
+		//receieve message
+		len = server.Recv(m_server,recv_buffer);
+		SplitString(recv_buffer,sp," ");
+		int recv_file_num = atoi(sp[0].c_str());
+		string recv_file_name;
+		int recv_file_size;
+		for(int i = 1; i <= recv_file_num; i++)
+		{
+			recv_file_name = sp[i*2-1];
+			recv_file_size = atoi(sp[i*2].c_str());
+		}
+		//next step
+
 	}
 
 	if(request_num = 3)
@@ -90,6 +130,24 @@ void init_request(int request_num)
 		total_message = sprintf(num_buffer,"%d%s",request_num,"/0");
 		total_message+ = sprintf(num_buffer + total_message, "%d%s",flr.file_name,"/0");
 		server.send(m_server,total_message,strlen(total_message));
+
+		//receive file
+		len = server.Recv(m_server,recv_buffer);
+		SplitString(recv_buffer,sp," ");
+		int recv_tmplist_size = atoi(sp[0].c_str);
+		int recv_end_point;
+		int recv_chunknum[strlen(sp)][10001];
+		for(int i = 1; i <= recv_tmplist_size; i++)
+		{
+			recv_end_point = atoi(sp[i*2-1].c_str());
+			for(int j = 0; j < strlen(sp[i*2]); j++)
+			{
+				recv_chunknum[i-1][j] = sp[i*2][j];
+			}
+
+		}
+
+
 	}
 
 	if(request_num = 4)
@@ -99,6 +157,12 @@ void init_request(int request_num)
 		total_message = sprintf(num_buffer,"%d%s",request_num,"/0");
 		total_message+ = sprintf(num_buffer + total_message, "%s%s%d%s",crr.file_name,"/0",crr.chunk_num,"/0");
 		server.send(m_server,total_message,strlen(total_message));
+
+		len = server.Recv(m_server,recv_buffer);
+		if(recv_buffer == "1")
+		{
+			cout << "Chunk register success!" << endl;
+		}
 	}
 
 	if(request_num = 5)
@@ -109,7 +173,33 @@ void init_request(int request_num)
 		total_message+ = sprintf(num_buffer + total_message, "%s%s%d%s",fcr.file_name,"/0",fcr.chunk_num,"/0");
 		server.send(target_peer,total_message,strlen(total_message));
 
+		//receive file
+		cout << "Start receive files!" << endl;
+		char file_buffer[1024] = {0};
+		int readLen = 0;
+		int readLen = 0;
+    	string desFileName = "peer_new_in";
+    	ofstream desFile;
+    	desFile.open(desFileName.c_str(), ios::binary);
+    	if (!desFile)
+    	{
+        	return ;
+    	}
+    	do 
+    	{
+        	readLen = recv(m_Client,buffer,bufferSize, 0);
+        	if (readLen == 0)
+        	{
+            	break;
+        	}
+        	else
+        	{
+            	desFile.write(buffer, readLen);
+        	}
+    	} while(true);
+    	desFile.close();
 	}
+
 }
 
 int main()
